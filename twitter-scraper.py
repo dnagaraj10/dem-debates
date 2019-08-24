115b51379954ce81f405037558c0eb984ba3e117
dnagaraj10 <31592319+dnagaraj10@users.noreply.github.com>
2019-08-24 09:43:20 -0500

    Update twitter-scraper.py

--- twitter-scraper.py
+++ twitter-scraper.py
@@ -1,8 +1,17 @@
+import tweepy
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
+import csv
+import authCodes #.py file with access keys
 
-auth = OAuthHandler(credentials.CONSUMER_KEY,credentials.CONSUMER_SECRET)
-auth.set_access_token(credentials.ACCESS_TOKEN,credentials.ACCESS_TOKEN_SECRET)
+auth = OAuthHandler(authCodes.CONSUMER_KEY,authCodes.CONSUMER_SECRET)
+auth.set_access_token(authCodes.ACCESS_TOKEN,authCodes.ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
+demDebateCSV = open('demdeb.csv', 'a')
+csvWriter = csv.writer(demDebateCSV)
+
+for tweet in tweepy.Cursor(api.search,q="#DemDebate",count=10000, lang="en", since="2019-07-25", tweet_mode='extended').items():
+    csvWriter.writerow([tweet.user.screen_name, tweet.text.encode('utf-8')])
  
