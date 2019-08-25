cfc02c9494506a2b72a98f15a1baab4da690deee
dnagaraj10 <31592319+dnagaraj10@users.noreply.github.com>
2019-08-25 08:41:45 -0500

    Update authCodes.py

--- authCodes.py
+++ authCodes.py
@@ -1,5 +1,5 @@
 count = 1
-API_KEY = '' #add Consumer Key here
-SECRET_KEY = '' #add Consumer Secret here
-ACCESS_TOKEN = '' #add Access Token here
-ACCESS_SECRET = '' #add Access Token Secret here
+API_KEY = ' ' #add Consumer Key here
+SECRET_KEY = ' ' #add Consumer Secret here
+ACCESS_TOKEN = ' ' #add Access Token here
+ACCESS_SECRET = ' ' #add Access Token Secret here
